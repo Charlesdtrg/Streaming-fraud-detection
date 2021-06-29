731400788de164ef003aaedaa4efee6397af720f
thousand-had <37281085+thousand-had@users.noreply.github.com>
2021-06-29 15:33:45 +0200

    Add files via upload
    working pipeline

--- quickstart/src/main/scala/org/myorg/quickstart/StreamingJob.scala
+++ quickstart/src/main/scala/org/myorg/quickstart/StreamingJob.scala
@@ -18,10 +18,25 @@
 
 package org.myorg.quickstart
 
+import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
+
 import java.util.Properties
-import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.serialization.{SerializationSchema, SimpleStringEncoder, SimpleStringSchema}
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
+import org.apache.flink.core.fs.Path
+import org.apache.flink.streaming.api.functions.sink.filesystem.{OutputFileConfig, StreamingFileSink}
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.streaming.api.windowing.time.Time
+
+import java.time.Duration
+import java.util.ArrayList
+import java.util.List
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -61,18 +76,110 @@ object StreamingJob {
      *
      */
 
-//     env.readTextFile(filePath= "/tmp/test_flink.txt")
+//    env.readTextFile(filePath= "/tmp/test_flink.txt")
 
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "localhost:9092")
     properties.setProperty("group.id", "test")
 
-    val stream = env
-      .addSource(new FlinkKafkaConsumer[String]("displays", new SimpleStringSchema(), properties))
+    val topics: List[String] = new ArrayList[String]()
+    topics.add("clicks")
+    topics.add("displays")
 
-    stream.print
+    // CONSUME STREAMS AND CONVERT TO JSON
+    // /!\ Simplestringschema converts command lines to string. Best to transform to json
+//    val stream = env
+//      .addSource(new FlinkKafkaConsumer[String](topics, new SimpleStringSchema(), properties))
+    val stream = env
+      .addSource(new FlinkKafkaConsumer[ObjectNode](topics, new JSONKeyValueDeserializationSchema(false), properties)
+        .assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness[ObjectNode](Duration.ofSeconds(20)))
+      )
+
+    // add streamFraud with topic "fraud"
+
+//    stream.print()
+
+    // FILTER
+
+//     CREATE SINK TO WRITE TO
+//    val outputPath = "../eventSink"
+//    val config = OutputFileConfig
+//      .builder()
+//      .withPartPrefix("5min_event")
+//      .withPartSuffix(".txt")
+//      .build()
+//    val sink: StreamingFileSink[String] = StreamingFileSink
+//      .forRowFormat(new Path(outputPath), new SimpleStringEncoder[String]("UTF-8"))
+//      .withRollingPolicy(
+//        DefaultRollingPolicy.builder()
+//          .withRolloverInterval(TimeUnit.MINUTES.toMillis(2)) // closes after X minutes
+//          .withInactivityInterval(TimeUnit.MINUTES.toMillis(1)) // closes after X minute of inactivity
+//          .withMaxPartSize(10 * 1024 * 1024) // Max size of 10 Mo
+//          .build())
+//      .withOutputFileConfig(config)
+//      .build()
+
+//    stream
+//      .map(x => x.toString())
+//      .addSink(sink)
+
+    // ADD KafkaProducer for fraud
+
+
+
+
+    // FORM WINDOW
+    val windowedStreamUID = stream
+      .map(x => (x.get("value").get("uid"), x.get("value").get("eventType"), 1))
+//      .map(x => (x,1))
+//      .keyBy(x => x.get("value").get("uid"))
+      .keyBy(x => x._1)
+      .window(SlidingEventTimeWindows.of(Time.minutes(2), Time.minutes(1)))
+      .reduce( (a, b) => (a._1, a._2, a._3+b._3) )
+      .filter(x => x._3 >= 20)
+//  val windowedStreamIP = stream
+//    .keyBy(x => x.get("value").get("IP"))
+//    .window(SlidingEventTimeWindows.of(Time.minutes(2), Time.minutes(1)))
+//
+    windowedStreamUID.print()
+
+//    val output_path = "./"
+//    windowedStream.writeAsText(output_path: String)
+//
+    // JOIN
+    // windowedStreamEvents = windowedStreamDisplay.join(windowedStreamDisplay, on="uid")
+
+    // FILTER THE FRAUDULENT DATA
+    // user clicks too often on the ads
+    // val fraud_case1_too_many_clicks = windowedStreamEvents.filter(nb_clicks>=15%)
+    // ...
+    // User clicks several time on the same ad
+    // same IP address has several users (join on IP ?)
+
+//      stream.addSink(sink)
+
 
     // execute program
     env.execute("Flink Streaming Scala API Skeleton")
   }
 }
+
+//    streamDisplay
+//      .flatMap(raw => JsonMethods.parse(raw).toOption)
+//      .map(_.extract[Car])
+//      .windowAll("uid")
+//      .TimeWindow(Time.minutes(1))
+
+
+// CODE CHARLES SINK
+//val sink: StreamingFileSink[String] = StreamingFileSink
+//  .forRowFormat(new Path(outputPath), new SimpleStringEncoder[String]("UTF-8"))
+//  .withRollingPolicy(
+//  DefaultRollingPolicy.builder()
+//  .withRolloverInterval(TimeUnit.MINUTES.toMillis(1))
+//  .withInactivityInterval(TimeUnit.MINUTES.toMillis(5))
+//  .withMaxPartSize(1024 * 1024 * 1024)
+//  .build())
+//  .build()
+//
+//  stream.addSink(sink)
